25a28b3748db5c8b6b3d5e934e3831a35af47f72
brightsummit.co <naseemoyebola@gmail.com>
2021-12-06 19:19:00 +0100

    Integrate Firebase

--- CCDemo/CCDemo/AppDelegate.swift
+++ CCDemo/CCDemo/AppDelegate.swift
@@ -2,10 +2,11 @@
 //  AppDelegate.swift
 //  CCDemo
 //
-//  Created by IBE on 06/12/2021.
+//  Created by naseem on 06/12/2021.
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        FirebaseApp.configure()
         return true
     }
 
