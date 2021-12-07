1229e107b43268bf18d9f283f1b978026c7ee6f2
brightsummit.co <naseemoyebola@gmail.com>
2021-12-07 14:33:47 +0100

    Receive Verification Code via SMS

--- CCDemo/CCDemo/Controllers/PhoneViewController.swift
+++ CCDemo/CCDemo/Controllers/PhoneViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class PhoneViewController: UIViewController {
+class PhoneViewController: UIViewController, UITextFieldDelegate {
+    private let phoneField: UITextField = {
+        let field = UITextField()
+        field.backgroundColor = .secondarySystemBackground
+        field.placeholder = "Phone Number"
+        field.returnKeyType = .continue
+        field.textAlignment = .center
+        return field
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(phoneField)
+        phoneField.frame = CGRect(x: 0, y: 0, width: 220, height: 50)
+        phoneField.center = view.center
+        phoneField.delegate = self
     }
 
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        if let text = textField.text, !text.isEmpty {
+            let number = "+1\(text)"
+            AuthManager.shared.startAuth(phoneNumber: number) { [weak self] success in
+                guard success else { return }
+                DispatchQueue.main.async {
+                     let vc = SMSViewController()
+                    vc.title = "Enter Code"
+                    self?.navigationController?.pushViewController(vc, animated: false)
+                }
+            }
+        }
+        return true
+    }
 }
 
--- CCDemo/CCDemo/Controllers/SMSViewController.swift
+++ CCDemo/CCDemo/Controllers/SMSViewController.swift
@@ -7,23 +7,40 @@
 
 import UIKit
 
-class SMSViewController: UIViewController {
+class SMSViewController: UIViewController, UITextFieldDelegate {
+    private let codeField: UITextField = {
+        let field = UITextField()
+        field.backgroundColor = .secondarySystemBackground
+        field.placeholder = "Phone Number"
+        field.returnKeyType = .continue
+        field.textAlignment = .center
+        return field
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(codeField)
+        codeField.frame = CGRect(x: 0, y: 0, width: 220, height: 50)
+        codeField.center = view.center
+        codeField.delegate = self
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        if let text = textField.text, !text.isEmpty {
+            let code = text
+            AuthManager.shared.verifyCode(smsCode: code) { [weak self] success in
+                guard success else { return }
+                DispatchQueue.main.async {
+                     let vc = AccountViewController()
+                    vc.modalPresentationStyle = .fullScreen
+                    self?.present(vc, animated: true)
+                }
+            }
+        }
+        return true
     }
-    */
 
 }
